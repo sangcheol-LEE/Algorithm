dd3f0d68beb5f7c0107369925f494e703fc7fd02
이상철 <89136968+sangcheol-LEE@users.noreply.github.com>
2023-04-21 15:40:43 +0900

    [unrated] Title: 직사각형 넓이 구하기, Time: 0.10 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
+++ 프로그래머스/unrated/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -1,25 +1,50 @@
-const solution = (arr) => {
-  let answer = 0;
-  let x = [];
-  let y = [];
+class Solution {
+  constructor(arr) {
+    this.arr = arr;
+  }
+  getX_axisArray() {
+    let x_axis_array = [];
+    this.arr.forEach((element) => {
+      x_axis_array.push(element[0]);
+    });
+    return x_axis_array;
+  }
+  getY_axisArray() {
+    let y_axis_array = [];
+    this.arr.forEach((element) => {
+      y_axis_array.push(element[1]);
+    });
+    return y_axis_array;
+  }
+  getMaxX_axis() {
+    return Math.max(...this.getX_axisArray());
+  }
+  getMinX_axis() {
+    return Math.min(...this.getX_axisArray());
+  }
+  getMaxY_axis() {
+    return Math.max(...this.getY_axisArray());
+  }
+  getMinY_axis() {
+    return Math.min(...this.getY_axisArray());
+  }
+  getTotalLength() {
+    return (
+      (this.getMaxX_axis() - this.getMinX_axis()) *
+      (this.getMaxY_axis() - this.getMinY_axis())
+    );
+  }
+}
 
-  arr.forEach((element) => {
-    x.push(element[0]);
-    y.push(element[1]);
-  });
-
-
-
-  const getMaxNumber = (arr) => {
-    return Math.max(...arr);
-  };
-
-  const getMinNumber = (arr) => {
-    return Math.min(...arr);
-  };
-
-  answer =
-    (getMaxNumber(x) - getMinNumber(x)) * (getMaxNumber(y) - getMinNumber(y));
-
-  return answer;
-};
+const ret = new Solution([
+  [1, 1],
+  [2, 1],
+  [2, 2],
+  [1, 2],
+]);
+const ret2 = new Solution([
+  [-1, -1],
+  [1, 1],
+  [1, -1],
+  [-1, 1],
+]);
